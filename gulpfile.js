cca798862efd4264548ffd55c1ddcac2428d8622
anton <you@example.com>
2020-05-18 15:13:59 +0300

    added header

--- gulpfile.js
+++ gulpfile.js
@@ -12,7 +12,7 @@ var sass = require('gulp-sass'),
 /* sass.compiler = require('node-sass'); */
 
 gulp.task('sass', function () {
-return gulp.src('app/scss/style.scss')
+return gulp.src('app/scss/**/*.scss')
 .pipe(sass({outputStyle: 'expanded'}))
 //.pipe(sass({outputStyle: 'compressed'}))
 .pipe(rename({suffix: '.min'}))
@@ -68,7 +68,7 @@ gulp.task('browser-sync', function() {
 //}); 
 
 gulp.task('watch', function(){
-    gulp.watch('app/scss/style.scss', gulp.parallel('sass'))
+    gulp.watch('app/scss/**/*.scss', gulp.parallel('sass'))
     gulp.watch('app/*html', gulp.parallel('html'))
     gulp.watch('app/js/*.js', gulp.parallel('js'))
 });
