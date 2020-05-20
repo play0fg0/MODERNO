2926e82fdd70459eed670cd2f5454abf188fc1d8
anton <you@example.com>
2020-05-20 10:21:37 +0300

    add slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,17 @@
 $(function(){
+
+    $(".rate-star").rateYo({
+        rating: 5,
+        readOnly: true,
+        starWidth: "12px"
+      });
+
+    $('.product-slider__inner').slick({
+        dots: true,
+        arrows:false,
+        slidesToShow: 3,
+        slidesToScroll: 3       
+    });
+
     var mixer = mixitup('.product__inner-box');
 });
